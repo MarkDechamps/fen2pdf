6ac36213453f6211ea3b6bc556f02900fbcf8dca
Mark Dechamps <mark.dechamps@gmail.com>
2024-06-29 16:02:20 +0200

    adding properties saved preferences+ finishing up

--- src/main/java/be/md/Fen2PdfMain.java
+++ src/main/java/be/md/Fen2PdfMain.java
@@ -2,26 +2,35 @@ package be.md;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
 import java.nio.file.Path;
+import java.util.Properties;
+import java.util.stream.Collectors;
 
 public class Fen2PdfMain {
+    private static final String CONFIG_FILE = "config.properties"; // Properties file name
+    private static Properties properties = new Properties();
     private static final JButton startButton = new JButton("Generate PDF");
     private static final SpinnerNumberModel spinnerModel = new SpinnerNumberModel(4, 1, 8, 1);
     private static final ScrollableTextImageList scrollableTextImageList = new ScrollableTextImageList();
     private static final Color BACKGROUND_COLOR = Color.LIGHT_GRAY;
     private static boolean inProgress = false;
     private static String workingDirectory;
+    private static final JButton selectDirButton= new JButton("Select working dir");
 
     public static void main(String[] args) {
 
         SwingUtilities.invokeLater(() -> {
+            loadProperties();
             createAndShowGUI();
-
             var pgns = PgnFileLister.listPgnFilesInCurrentDirectory(".");
-            scrollableTextImageList.addItem("Pgn files to process:\n");
-            pgns.forEach(pgn -> {
-                scrollableTextImageList.addItem(pgn.toString());
-            });
+
+
+            String commaSeparatedPaths = pgns.stream()
+                    .map(Path::toString)
+                    .collect(Collectors.joining(", "));
+
+            scrollableTextImageList.addItem("Pgn files to process:\n"+commaSeparatedPaths);
         });
 
 
@@ -46,14 +55,43 @@ public class Fen2PdfMain {
         rootPanel.setLayout(new BoxLayout(rootPanel, BoxLayout.Y_AXIS));
         rootPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
-        // Button panel with centered alignment
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        buttonPanel.setBackground(BACKGROUND_COLOR);
-        buttonPanel.add(startButton);
-        buttonPanel.add(Box.createHorizontalStrut(20)); // Add horizontal spacing between buttons
-        JButton selectDirButton = new JButton("Select working dir");
+
+        JPanel spinnerPanel = selectNrDiagramsPerRow();
+        rootPanel.add(spinnerPanel);
+
+
+        rootPanel.add(Box.createVerticalStrut(10));
+
+        JPanel dirSelectionPanel = selectWorkingDir(frame);
+        rootPanel.add(dirSelectionPanel);
+
+        rootPanel.add(Box.createVerticalStrut(10));
+
+        JScrollPane scrollPane = new JScrollPane(scrollableTextImageList);
+        scrollPane.setBackground(BACKGROUND_COLOR);
+        scrollableTextImageList.setBackground(BACKGROUND_COLOR);
+        scrollPane.setPreferredSize(new Dimension(600, 500));
+        rootPanel.add(scrollPane);
+
+
+        JPanel startButtonPanel = generatePDFButton();
+        rootPanel.add(startButtonPanel);
+
+        frame.add(rootPanel);
+        frame.setVisible(true);
+    }
+
+    private static JPanel generatePDFButton() {
+        JPanel startButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        startButtonPanel.setBackground(BACKGROUND_COLOR);
+        startButtonPanel.add(startButton);
+        return startButtonPanel;
+    }
+
+    private static JPanel selectWorkingDir(JFrame frame) {
         selectDirButton.addActionListener(e -> {
             JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File(workingDirectory));
             fileChooser.setDialogTitle("Select working directory (for input pgn and output pdf files)");
             fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 
@@ -61,36 +99,27 @@ public class Fen2PdfMain {
             if (inputFolder == JFileChooser.APPROVE_OPTION) {
                 String selectedDir = fileChooser.getSelectedFile().getAbsolutePath();
                 workingDirectory = selectedDir;
+                selectDirButton.setText(workingDirectory); // Update label text
                 JOptionPane.showMessageDialog(frame, "Selected Directory: " + selectedDir);
+                saveProperties();
             }
         });
-        buttonPanel.add(selectDirButton);
-        rootPanel.add(buttonPanel);
+        JPanel dirSelectionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        dirSelectionPanel.add(new JLabel("Select directory:"));
+        dirSelectionPanel.add(selectDirButton);
+        dirSelectionPanel.setBackground(BACKGROUND_COLOR);
+        return dirSelectionPanel;
+    }
 
-        // Add padding between button panel and spinner panel
-        rootPanel.add(Box.createVerticalStrut(10));
-
+    private static JPanel selectNrDiagramsPerRow() {
         // Spinner panel
-        JPanel spinnerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JPanel spinnerPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         spinnerPanel.setBackground(BACKGROUND_COLOR);
         JSpinner spinner = new JSpinner(spinnerModel);
         spinner.setPreferredSize(new Dimension(50, 30));
         spinnerPanel.add(new JLabel("How many diagrams per row? (1-8): "));
         spinnerPanel.add(spinner);
-        rootPanel.add(spinnerPanel);
-
-        // Add padding between spinner panel and scroll pane
-        rootPanel.add(Box.createVerticalStrut(10));
-
-        // Scroll pane
-        JScrollPane scrollPane = new JScrollPane(scrollableTextImageList);
-        scrollPane.setBackground(BACKGROUND_COLOR);
-        scrollableTextImageList.setBackground(BACKGROUND_COLOR);
-        scrollPane.setPreferredSize(new Dimension(600, 500));
-        rootPanel.add(scrollPane);
-
-        frame.add(rootPanel);
-        frame.setVisible(true);
+        return spinnerPanel;
     }
 
     private static void putIconOn(JFrame frame) {
@@ -112,8 +141,8 @@ public class Fen2PdfMain {
 
         @Override
         protected Void doInBackground() {
-            Path location= Path.of(workingDirectory);
-            ChessBoardPDFGenerator.process(new Feedback(scrollableTextImageList), PgnFileLister.listPgnFilesInCurrentDirectory(workingDirectory),diagramsPerRow,location);
+            Path location = Path.of(workingDirectory);
+            ChessBoardPDFGenerator.process(new Feedback(scrollableTextImageList), PgnFileLister.listPgnFilesInCurrentDirectory(workingDirectory), diagramsPerRow, location);
             return null;
         }
 
@@ -122,6 +151,37 @@ public class Fen2PdfMain {
             setBusyState(false);
             startButton.setEnabled(true);
             JOptionPane.showMessageDialog(null, "PDF generation completed successfully!", "Success", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+    private static void saveProperties() {
+        try (OutputStream output = new FileOutputStream(CONFIG_FILE)) {
+            properties.setProperty("workingDirectory", workingDirectory);
+            properties.store(output, "FEN2PDF Application Properties");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void setDefaultProperties() {
+        try (OutputStream output = new FileOutputStream(CONFIG_FILE)) {
+            properties.setProperty("workingDirectory", "");
+            properties.store(output, "FEN2PDF Application Properties (Default)");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void loadProperties() {
+        try (InputStream input = new FileInputStream(CONFIG_FILE)) {
+            properties.load(input);
+            workingDirectory = properties.getProperty("workingDirectory", "");
+            if (workingDirectory.isEmpty()) {
+                workingDirectory = ".";
+            }
+            selectDirButton.setText(workingDirectory);
+        } catch (IOException ex) {
+            setDefaultProperties();
         }
     }
 }
