307c5951122d06ec8d4d0066990b1644fb2cb4d9
Mark Dechamps <mark.dechamps@gmail.com>
2024-11-15 15:44:13 +0100

    fix tests

--- src/test/java/be/md/MirrorFenVerticallyTest.java
+++ src/test/java/be/md/MirrorFenVerticallyTest.java
@@ -7,37 +7,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MirrorFenVerticallyTest {
     @Test
-    public void testMirror() {
-        var fen = new Fen("r3k2r/pppqppbp/2n2np1/4P3/3P4/2N2NP1/PPPQPPBP/R3K2R w KQkq - 0 1");
-        assertEquals(new Fen("r2k3r/pbppqppp/1pn2n2/3P4/4P3/1PN2N2/PBPPQPPP/R2K3R w KQkq - 0 1"), fen.mirrorVertically());
-    }
-
-    @Test
     public void shouldMirrorAndFlip() {
         var fen = "r3k2r/pppqppbp/2n2np1/4P3/3P4/2N2NP1/PPPQPPBP/R3K2R w KQkq - 0 1";
 
-        var maf = "r3k2r" +
+        var maf = "r3k2r/" +
                         "pppqppbp/" +
                         "2n2np1/" +
                         "3p4/" +
                         "4p3/" +
                         "2N2NP1/" +
                         "PPPQPPBP/" +
-                        "R3K2R/" +
-                        " z kqKQ - 0 1";
+                        "R3K2R" +
+                        " b KQkq - 0 1";
 
-        assertEquals(maf, new Fen(fen).mirrorAndFlip());
+        assertEquals(new Fen(maf), new Fen(fen).mirrorAndFlip());
     }
     @Test
     public void shouldMirror1Line() {
         var fen = "r3k2r w KQkq - 0 1";
-        var maf = "R3K2R b kqKQ - 0 1";
-        assertEquals(maf, new Fen(fen).mirrorAndFlip());
+        var maf = "R3K2R b KQkq - 0 1";
+        assertEquals(new Fen(maf), new Fen(fen).mirrorAndFlip());
     }
     @Test
     public void shouldMirrorMultipleLines() {
         var fen = new Fen("r3k2r/pppqppbp/2n2np1/4P3/3P4/2N2NP1/PPPQPPBP/R3K2R w KQkq - 0 1");
-        var maf = "r3k2r/" +
+        var maf = new Fen("r3k2r/" +
                 "pppqppbp/" +
                 "2n2np1/" +
                 "3p4/" +
@@ -45,7 +39,7 @@ class MirrorFenVerticallyTest {
                 "2N2NP1/" +
                 "PPPQPPBP/" +
                 "R3K2R" +
-                " b KQkq - 0 1";
+                " b KQkq - 0 1");
 
         assertEquals(maf, fen.mirrorAndFlip());
     }
